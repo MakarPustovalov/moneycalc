3d08de3c20b5b4bb13ce4c0cab3d749185ad2a96
MakarPustovalov <makar.pht@gmail.com>
2020-08-15 19:57:18 +0300

    Updates

---
+++ precache-manifest.eacc7c2621a732862169c55596a040ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cc2e61acd534091ce12ff1c7856b7ae",
+    "url": "/makarpustovalov.io/moneycalc/index.html"
+  },
+  {
+    "revision": "a6d0b1bb15dfcd546f31",
+    "url": "/makarpustovalov.io/moneycalc/static/css/main.9e034465.chunk.css"
+  },
+  {
+    "revision": "9b52fe5c735ed7610a79",
+    "url": "/makarpustovalov.io/moneycalc/static/js/2.177efff1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/makarpustovalov.io/moneycalc/static/js/2.177efff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6d0b1bb15dfcd546f31",
+    "url": "/makarpustovalov.io/moneycalc/static/js/main.4e92ccb7.chunk.js"
+  },
+  {
+    "revision": "7efbb3c6ca1398bb8297",
+    "url": "/makarpustovalov.io/moneycalc/static/js/runtime-main.add591c6.js"
+  }
+]);
